278199cd070702990f2c91fb8a98c2cb9ee8ee49
FoxWorn3365 <fede.cosma.ge@gmail.com>
2024-04-22 17:44:31 +0200

    fixed VSshit

--- UncomplicatedCustomItems/Events/Internal/Server.cs
+++ UncomplicatedCustomItems/Events/Internal/Server.cs
@@ -17,43 +17,6 @@ namespace UncomplicatedCustomItems.Events.Internal
         /// <summary>
         /// Spawn items on round started
         /// </summary>
-<<<<<<< HEAD
         private static void SpawnItemsOnRoundStarted() { }
-=======
-        private static void SpawnItemsOnRoundStarted()
-        {
-            SpawnCustomItemsList(Plugin.Instance.Config.CustomItems.Values);
-            SpawnCustomItemsList(Plugin.Instance.Config.CustomArmors.Values);
-            SpawnCustomItemsList(Plugin.Instance.Config.CustomWeapons.Values);
-            SpawnCustomItemsList(Plugin.Instance.Config.CustomKeycards.Values);
-        }
-
-        /// <summary>
-        /// Spawn items in enumerable
-        /// </summary>
-        /// <param name="customThings"></param>
-        private static void SpawnCustomItemsList(IEnumerable<SerializableThing> customThings)
-        {
-            foreach (var customItem in customThings)
-            {
-                foreach (var spawnPoint in customItem.SpawnPoint)
-                {
-                    if (customItem.SpawnPoint is null || spawnPoint.Chance == 0)
-                    {
-                        continue;
-                    }
-
-                    var chance = UnityEngine.Random.Range(0, 100);
-
-                    if (spawnPoint.Chance != 100 && spawnPoint.Chance > chance)
-                    {
-                        continue;
-                    }
-
-                    customItem.Create(null).Spawn(spawnPoint.Location.GetPosition() + spawnPoint.Position);
-                }            
-            }
-        }
->>>>>>> 2ff79ee1861a7035b48d099ce36d6c27c501525a
     }
 }
