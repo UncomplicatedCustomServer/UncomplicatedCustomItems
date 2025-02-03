6bf72269f50d302369167f912cc0ff2997cc53f2
Mr. Baguetter <Mr.baguetter1@gmail.com>
2025-02-03 12:02:32 -0600

    Add files via upload

---
+++ UncomplicatedCustomItems/Interfaces/SpecificData/IWeaponData.cs
@@ -0,0 +1,11 @@
+﻿namespace UncomplicatedCustomItems.Interfaces.SpecificData
+{
+    public interface IWeaponData : IData
+    {
+        public abstract float Damage { get; set; }
+
+        public abstract float FireRate { get; set; }
+
+        public abstract byte MaxAmmo { get; set; }
+    }
+}
