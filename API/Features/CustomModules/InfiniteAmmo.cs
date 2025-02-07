1e674326596efacddfd1a30a81c025a11f0de50b
Mr. Baguetter <Mr.baguetter1@gmail.com>
2025-02-06 21:29:27 -0600

    Delete API directory

--- API/Features/CustomModules/InfiniteAmmo.cs
+++
@@ -1,9 +0,0 @@
-using UncomplicatedCustomItems.Enums;
-
-namespace UncomplicatedCustomItems.API.Features.CustomModules
-{
-    public class InfiniteAmmo : CustomModule
-    {
-        public new static CustomFlags Flag => CustomFlags.InfiniteAmmo;
-    }
-}
--- API/Features/SpecificData/Data.cs
+++
@@ -1,8 +0,0 @@
-﻿using UncomplicatedCustomItems.Interfaces.SpecificData;
-
-namespace UncomplicatedCustomItems.API.Features.SpecificData
-{
-    public class Data : IData
-    {
-    }
-}
