6f5056a117384e19eb2f83547e3aba3c1cc1d274
Mr. Baguetter <Mr.baguetter1@gmail.com>
2025-02-06 21:30:01 -0600

    Delete Interfaces directory

--- Interfaces/SpecificData/IMedikitData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UncomplicatedCustomItems.Interfaces.SpecificData
-{
-    public interface IMedikitData
-    {
-        public abstract float Health { get; set; }
-
-        public abstract bool MoreThanMax { get; set; }
-    }
-}
