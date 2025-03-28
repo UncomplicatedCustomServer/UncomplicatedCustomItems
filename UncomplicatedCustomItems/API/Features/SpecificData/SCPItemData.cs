8994be3b7bb75df3b896d3f423aeac89cb3dd54b
Mr. Baguetter <mr.baguetter1@gmail.com>
2025-03-28 00:22:37 -0500

    Adding custom data to SCP custom items

--- UncomplicatedCustomItems/API/Features/SpecificData/SCPItemData.cs
+++ UncomplicatedCustomItems/API/Features/SpecificData/SCPItemData.cs
@@ -3,11 +3,16 @@ using UncomplicatedCustomItems.Interfaces.SpecificData;
 
 namespace UncomplicatedCustomItems.API.Features.SpecificData
 {
+    public class SCPItemData : Data, ISCPItemData
+    {
+
+    }
     public class SCP500Data : ISCP500Data
     {
         public EffectType Effect { get; set; } = new();
         public float Duration { get; set; } = 20;
         public byte Intensity { get; set; } = 1;
+        public bool HealPlayer { get; set; } = false;
     }
 
     public class SCP207Data : ISCP207Data
@@ -21,44 +26,44 @@ namespace UncomplicatedCustomItems.API.Features.SpecificData
     public class SCP018Data : ISCP018Data
     {
         public float FriendlyFireTime { get; set; } = 2f;
-
         public float FuseTime { get; set; } = 2f;
-
     }
 
-    public class SCP268Data : ISCP268Data
-    {
-        
-    }
     public class SCP330Data : ISCP330Data
     {
-        
+
     }
 
     public class SCP2176Data : ISCP2176Data
     {
         public float FuseTime { get; set; } = 2f;
-
     }
     public class SCP244aData : ISCP244aData
     {
-        
+        public float ActivationDot { get; set; } = 1f;
+        public float Health { get; set; } = 1f;
+        public float MaxDiameter { get; set; } = 1f;
+        public bool Primed { get; set; } = false;
+
     }
     public class SCP244bData : ISCP244bData
     {
-        
+        public float ActivationDot { get; set; } = 1f;
+        public float Health { get; set; } = 1f;
+        public float MaxDiameter { get; set; } = 1f;
+        public bool Primed { get; set; } = false;
     }
 
     public class SCP1853Data : ISCP1853Data
     {
-        
+        public EffectType Effect { get; set; } = new();
+        public float Duration { get; set; } = 20;
+        public byte Intensity { get; set; } = 1;
     }
     public class SCP1576Data : ISCP1576Data
     {
-        
-    }
-    public class SCP1344Data : ISCP1344Data
-    {
-        
+        public EffectType Effect { get; set; } = new();
+        public float Duration { get; set; } = 20;
+        public byte Intensity { get; set; } = 1;
     }
 }
--- UncomplicatedCustomItems/Interfaces/SpecificData/ISCPItemData.cs
+++ UncomplicatedCustomItems/Interfaces/SpecificData/ISCPItemData.cs
@@ -1,3 +1,5 @@
+using Exiled.API.Enums;
+
 namespace UncomplicatedCustomItems.Interfaces.SpecificData
 {
     public interface ISCPItemData : IData
@@ -6,12 +8,18 @@ namespace UncomplicatedCustomItems.Interfaces.SpecificData
     }
     public interface ISCP500Data : ISCPItemData
     {
-        
+        public abstract EffectType Effect { get; set; }
+        public abstract float Duration { get; set; }
+        public abstract byte Intensity { get; set; }
+        public abstract bool HealPlayer { get; set; }
     }
 
     public interface ISCP207Data : ISCPItemData
     {
-        
+        public abstract EffectType Effect { get; set; }
+        public abstract float Duration { get; set; }
+        public abstract byte Intensity { get; set; }
+        public abstract bool ApplyBaseEffect { get; set; }
     }
     public interface ISCP018Data : ISCPItemData
     {
@@ -20,13 +28,9 @@ namespace UncomplicatedCustomItems.Interfaces.SpecificData
         public abstract float FuseTime { get; set; }
     }
 
-    public interface ISCP268Data : ISCPItemData
-    {
-        
-    }
     public interface ISCP330Data : ISCPItemData
     {
-        
+
     }
 
     public interface ISCP2176Data : ISCPItemData
@@ -35,23 +39,29 @@ namespace UncomplicatedCustomItems.Interfaces.SpecificData
     }
     public interface ISCP244aData : ISCPItemData
     {
-        
+        public abstract float ActivationDot { get; set; }
+        public abstract float Health { get; set; }
+        public abstract float MaxDiameter { get; set; }
+        public abstract bool Primed { get; set; }
     }
     public interface ISCP244bData : ISCPItemData
     {
-        
+        public abstract float ActivationDot { get; set; }
+        public abstract float Health { get; set; }
+        public abstract float MaxDiameter { get; set; }
+        public abstract bool Primed { get; set; }
     }
 
     public interface ISCP1853Data : ISCPItemData
     {
-        
+        public abstract EffectType Effect { get; set; }
+        public abstract float Duration { get; set; }
+        public abstract byte Intensity { get; set; }
     }
     public interface ISCP1576Data : ISCPItemData
     {
-        
-    }
-    public interface ISCP1344Data : ISCPItemData
-    {
-        
+        public abstract EffectType Effect { get; set; }
+        public abstract float Duration { get; set; }
+        public abstract byte Intensity { get; set; }
     }
 }
