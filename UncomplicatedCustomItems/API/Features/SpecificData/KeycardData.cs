558010057b923c69fa1e44ffeef584301755029f
Mr. Baguetter <mr.baguetter1@gmail.com>
2025-04-25 18:48:05 -0500

    Prototyping new keycards

--- UncomplicatedCustomItems/API/Features/SpecificData/KeycardData.cs
+++ UncomplicatedCustomItems/API/Features/SpecificData/KeycardData.cs
@@ -12,5 +12,15 @@ namespace UncomplicatedCustomItems.API.Features.SpecificData
         /// Gets or sets the <see cref="KeycardPermissions"/> of the KeyCard
         /// </summary>
         public KeycardPermissions Permissions { get; set; } = KeycardPermissions.Checkpoints;
+
+        /*
+         * public string Name { get; set; } = "%owner%";
+         * public KeycardType { get set; } = KeycardType.ChaosInsurgency;
+         * public string TintColor { get; set; } = "%owner%";
+         * public int WearState { get; set; } = "%owner%";
+         * public string Rank { get; set; } = "Private";
+         * public string Label { get; set; } = "Label";
+         * public int SerialNumber { get; set; } = "1";
+        */
     }
 }
