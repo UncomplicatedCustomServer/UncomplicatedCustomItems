313142ca6eb64b8d791a90162ce0219ad0b9d0a0
Mr. Baguetter <mr.baguetter1@gmail.com>
2025-04-29 17:05:45 -0500

    Fixed ServerNamePatch

--- UncomplicatedCustomItems/HarmonyElements/Patches/ServerNamePatch.cs
+++ UncomplicatedCustomItems/HarmonyElements/Patches/ServerNamePatch.cs
@@ -14,10 +14,7 @@ namespace UncomplicatedCustomItems.HarmonyElements.Patches
                 LogManager.Debug("ServerTracking in config is set to false aborting ServerName patch.");
                 return;
             }
-
-            FieldInfo ServerName = AccessTools.Field(typeof(ServerConsole), "_serverName");
-            string CurrentName = (string)ServerName.GetValue(null);
-            ServerName.SetValue(null, CurrentName + $"<color=#00000000><size=1>UCI {Plugin.Instance.Version.ToString(3)}</size></color>");
+            ServerConsole.ServerName += $"<color=#00000000><size=1>UCI {Plugin.Instance.Version.ToString(3)}</size></color>";
             LogManager.Debug("ServerName Patched!");
         }
     }
