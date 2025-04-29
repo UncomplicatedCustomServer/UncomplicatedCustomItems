96ce3f7205786e19e0f28c9e70f808008687f9f6
Mr. Baguetter <mr.baguetter1@gmail.com>
2025-04-29 10:55:04 -0500

    ConsumablePatch will hopefully not crash servers now.

--- UncomplicatedCustomItems/HarmonyElements/Patches/ConsumablePatch.cs
+++ UncomplicatedCustomItems/HarmonyElements/Patches/ConsumablePatch.cs
@@ -1,21 +1,36 @@
 ﻿using HarmonyLib;
 using InventorySystem.Items.Usables;
+using UncomplicatedCustomItems.API;
 using UncomplicatedCustomItems.API.Features;
+using UncomplicatedCustomItems.API.Features.Helper;
 
-// Is this even being used?
-
-/*namespace UncomplicatedCustomItems.HarmonyElements.Patches
+namespace UncomplicatedCustomItems.HarmonyElements.Patches
 {
     [HarmonyPatch(typeof(Consumable), nameof(Consumable.ActivateEffects))]
-    internal class ConsumablePatch
+    public class ConsumablePatch
     {
-        static bool Prefix(Consumable __instance)
+        [HarmonyPrefix]
+        public static bool Prefix(Consumable __instance)
         {
-            if (SummonedCustomItem.TryGet(__instance.ItemSerial, out SummonedCustomItem summonedCustomItem))
-                return !summonedCustomItem.HandleCustomAction(__instance);
-
-            return true;
+            if (Utilities.TryGetSummonedCustomItem(__instance.ItemSerial, out SummonedCustomItem CustomItem))
+            {
+                LogManager.Debug($"Checking if {CustomItem.CustomItem.Name} is Adrenaline, Painkillers or Medkit");
+                if (CustomItem.Item.Type == ItemType.Adrenaline || CustomItem.Item.Type == ItemType.Painkillers || CustomItem.Item.Type == ItemType.Medkit)
+                {
+                    LogManager.Debug($"{CustomItem.CustomItem.Name} is Adrenaline, Painkillers or Medkit\n Applying patch...");
+                    return false;
+                }
+                else
+                {
+                    LogManager.Debug($"{CustomItem.CustomItem.Name} is not Adrenaline, Painkillers or Medkit\n Aborting patch...");
+                    return true;
+                }
+            }
+            else
+            {
+                LogManager.Debug($"{__instance.Name} Is not a UCI CustomItem!\n Aborting patch...");
+                return true;
+            }
         }
     }
 }
-*/
