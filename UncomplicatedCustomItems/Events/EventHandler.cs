602c6e8000de9411c5e6bc69e462090afbff3111
Mr. Baguetter <mr.baguetter1@gmail.com>
2025-05-08 19:09:46 -0500

    Harmony

--- UncomplicatedCustomItems/Events/EventHandler.cs
+++ UncomplicatedCustomItems/Events/EventHandler.cs
@@ -380,27 +380,12 @@ namespace UncomplicatedCustomItems.Events
                 {
                     _ToolGunSettings = new SettingBase[]
                     {
-                        new HeaderSetting("UCI ToolGun Settings", hintDescription: "If multiple are created the first one will be the correct one"),
+                        new HeaderSetting("UCI ToolGun Settings", hintDescription: "If multiple are created any will work"),
                         new UserTextInputSetting(21, "Primitive Color", placeHolder: "255, 0, 0, -1",  hintDescription: "The color of the primitives spawned by the ToolGun"),
                         new TwoButtonsSetting(22, "Deletion Mode", "ADS", "FlashLight Toggle", hintDescription: "Sets the deletion mode of the ToolGun"),
                         new TwoButtonsSetting(23, "Delete Primitives when unequipped?", "Yes", "No")
                     };
-                    HashSet<int> toolGunSettingIds = [];
-                    foreach (SettingBase toolGunSetting in _ToolGunSettings)
-                    {
-                        if (toolGunSetting.Id != 0)
-                            toolGunSettingIds.Add(toolGunSetting.Id);
-                    }
-                    foreach (int id in toolGunSettingIds)
-                    {
-                        if (!SettingBase.TryGetSetting(ev.Player, id, out _))
-                        {
-                            SettingBase.Register(ev.Player, _ToolGunSettings);
-                            foreach (ServerSpecificSettingBase settings in ServerSpecificSettingsSync.DefinedSettings) // Debugging
-                                LogManager.Debug($"{settings.Label} - {settings.SettingId}");
-                            break;
-                        }
-                    }
+                    SettingBase.Register(ev.Player, _ToolGunSettings);
                     StartRelativePosCoroutine(ev.Player);
                 }
             }
