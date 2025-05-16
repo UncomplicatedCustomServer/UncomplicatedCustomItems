a085df83588504844f340c9bb62cac5076798b52
Mr. Baguetter <mr.baguetter1@gmail.com>
2025-05-15 23:49:09 -0500

    Pickups now spawn

---
+++ UncomplicatedCustomItems/Extensions/PickupExtensions.cs
@@ -0,0 +1,20 @@
+﻿using LabApi.Features.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UncomplicatedCustomItems.Extensions
+{
+    internal static class PickupExtensions
+    {
+        public static Pickup CreateAndSpawn(this ItemType item, Vector3 pos)
+        {
+            Pickup pickup = Pickup.Create(item, pos);
+            pickup.Spawn();
+            return pickup;
+        }
+    }
+}
