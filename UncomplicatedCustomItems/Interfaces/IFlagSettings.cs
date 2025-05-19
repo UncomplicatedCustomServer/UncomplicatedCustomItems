8e3ce7454cb0309590f258387a735b547930d8cf
Mr. Baguetter <mr.baguetter1@gmail.com>
2025-05-19 11:28:20 -0500

    Cleanup

--- UncomplicatedCustomItems/Interfaces/IFlagSettings.cs
+++ UncomplicatedCustomItems/Interfaces/IFlagSettings.cs
@@ -6,38 +6,10 @@ namespace UncomplicatedCustomItems.Interfaces
     #nullable enable
     public interface IFlagSettings
     {
-        /*
-        public abstract string GlowColor { get; set; }
-        */
         public abstract List<ItemGlowSettings?> ItemGlowSettings { get; set; }
-        /*
-        public abstract float LifeStealAmount { get; set; }
-
-        public abstract float LifeStealPercentage { get; set; }
-        */
         public abstract List<LifeStealSettings?> LifeStealSettings { get; set; }
-        /*
-        public abstract string EffectEvent { get; set; }
-
-        public abstract EffectType Effect {get; set; }
-
-        public abstract byte EffectIntensity { get; set; }
-
-        public abstract float EffectDuration {get; set; }
-        */
-
         public abstract List<EffectSettings?> EffectSettings { get; set; }
-        /*
-        public abstract string? AudioPath { get; set; }
-
-        public abstract float? AudibleDistance { get; set; }
-
-        public abstract float? SoundVolume { get; set; }
-        */
         public abstract List<AudioSettings?> AudioSettings { get; set; }
-        /*
-        public abstract float? DamageRadius { get; set; }
-        */
         public abstract List<ExplosiveBulletsSettings?> ExplosiveBulletsSettings { get; set; }
         public abstract List<SpawnItemWhenDetonatedSettings?> SpawnItemWhenDetonatedSettings { get; set; }
         public abstract List<ClusterSettings?> ClusterSettings { get; set; }
