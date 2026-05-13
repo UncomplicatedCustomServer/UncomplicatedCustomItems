c66d84e2ba71f748fbd5d030e4c2ae3771efa80e
Mr-Baguetter <Mr.Baguetter1@gmail.com>
2026-05-13 18:26:32 -0500

    Fix: Fixed TriggerOn Flags

--- UncomplicatedCustomItems/API/CustomModuleAPI/CustomModules/Enums/TriggerOn.cs
+++ UncomplicatedCustomItems/API/CustomModuleAPI/CustomModules/Enums/TriggerOn.cs
@@ -5,14 +5,15 @@ namespace UncomplicatedCustomItems.API.CustomModuleAPI.CustomModules.Enums
     [Flags]
     public enum TriggerOn
     {
-        OnShot = 1,
-        OnUse = 2,
-        OnReload = 3,
-        OnChangedItem = 4,
-        OnAdded = 5,
-        OnDropped = 6,
-        OnDeath = 7,
-        OnHurt = 8,
-        OnDoorInteracted = 9,
+        None = 0,
+        OnShot = 1 << 0,
+        OnUse = 1 << 1,
+        OnReload = 1 << 2,
+        OnChangedItem = 1 << 3,
+        OnAdded = 1 << 4,
+        OnDropped = 1 << 5,
+        OnDeath = 1 << 6,
+        OnHurt = 1 << 7,
+        OnDoorInteracted = 1 << 8,
     }
 }
